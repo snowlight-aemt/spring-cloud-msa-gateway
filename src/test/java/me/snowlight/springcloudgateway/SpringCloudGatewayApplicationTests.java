9a7ff17985f0fa005fbb056c9494003cf052c659
private desktop pc <it.s.aemt@gmail.com>
2022-07-02 18:12:35 +0900

    Init

---
+++ src/test/java/me/snowlight/springcloudgateway/SpringCloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package me.snowlight.springcloudgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

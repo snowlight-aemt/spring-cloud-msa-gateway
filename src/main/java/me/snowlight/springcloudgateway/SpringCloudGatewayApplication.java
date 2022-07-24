5e2d21634ed0855ce1e385720d4bb7a0c0d23453
private desktop pc <it.s.aemt@gmail.com>
2022-07-24 17:46:00 +0900

    Config server 설정

--- src/main/java/me/snowlight/springcloudgateway/SpringCloudGatewayApplication.java
+++ src/main/java/me/snowlight/springcloudgateway/SpringCloudGatewayApplication.java
@@ -1,13 +1,21 @@
 package me.snowlight.springcloudgateway;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringCloudGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCloudGatewayApplication.class, args);
+	}
+
+	@Bean
+	public HttpTraceRepository httpTraceRepository() {
+		return new InMemoryHttpTraceRepository();
 	}
 
 }
